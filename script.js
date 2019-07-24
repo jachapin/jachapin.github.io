c76a02f5969869609c95ea1548500cd7de7b9f37
jachapin <32234320+jachapin@users.noreply.github.com>
2019-07-23 23:52:08 -0500

    update

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ https://jachapin.github.io
 2019
 */
 
+/* Fade in the description */
+$('#description').hide().fadeIn(1500);
+
+/* On mouse hover, change color of list items */
+$('li').hover(changeColor, removeColor);
+
+function changeColor() {
+    $(this).addClass('secondColor');
+}
+
+function removeColor() {
+    $(this).removeClass('secondColor');
+}
+
+
+
+
